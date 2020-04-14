8498d8131f5d5d78e3e2d03122ab1947431c9db8
repelliuss <officialrepelliuss@gmail.com>
2020-04-14 22:31:04 +0300

    Created main class SVE
    This class will control main flow of SVE system.

---
+++ src/sve/core/SVE.java
@@ -0,0 +1,28 @@
+package sve.core;
+
+/**
+ *Main class for SVE system
+ *
+ *@author repelliuss
+ */
+public class SVE {
+
+	public void start() {
+
+		init();
+		update();
+		finish();
+	}
+
+	private void init() {
+		//TODO
+	}
+
+	private void update() {
+		//TODO
+	}
+
+	private void finish() {
+		//TODO
+	}
+}
