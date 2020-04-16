cc985a6fd75229bdf8c83ac1d9560b88e7420032
repelliuss <officialrepelliuss@gmail.com>
2020-04-16 22:51:20 +0300

    Added test recorders
    These recorders will be used to register unit tests. There is also an
    example that shows how to do it in CoreTestRecorder.java .

---
+++ test/sve/test/stats/StatsTestRecorder.java
@@ -0,0 +1,22 @@
+package sve.test.stats;
+
+import sve.test.Test;
+import sve.test.TestManager;
+
+/**
+ * Utility class to register the
+ * tests of Stats module of
+ * SVE system to TestManager
+ *
+ * @author repelliuss
+ */
+public class StatsTestRecorder {
+
+	/**
+	 * Register your unit tests here!
+	 */
+	public static void recordAll(TestManager tester) {
+
+	}
+
+}
