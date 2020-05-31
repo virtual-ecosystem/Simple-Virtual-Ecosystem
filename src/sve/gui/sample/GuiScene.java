90211d0f659ae51222aba0e970e33b2f2be4f466
Harun Albayrak <52447324+harunalbayrak@users.noreply.github.com>
2020-05-31 21:22:44 +0300

    Added the Timer for updating to GuiScene class

--- src/sve/gui/sample/GuiScene.java
+++ src/sve/gui/sample/GuiScene.java
@@ -1,5 +1,6 @@
 package sve.gui.sample;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -12,10 +13,21 @@ import java.io.IOException;
 
 public class GuiScene extends Application implements EventHandler<ActionEvent>,environmentConstants{
     private FirstScene firstScene = new FirstScene(new Stage());
+    private SecondScene secondScene = new SecondScene();
+
+    private AnimationTimer timer = new AnimationTimer() {
+        @Override
+        public void handle(long now) {
+            //for update
+        }
+    };
 
     @Override
     public void start(Stage primaryStage) {
         primaryStage = firstScene.getStage();
+
+        secondScene.secondSceneCreator(getTimer());
+        firstScene.firstSceneCreator(secondScene.getScene());
 
         primaryStage.setScene(firstScene.getScene());
         primaryStage.show();
@@ -28,6 +40,10 @@ public class GuiScene extends Application implements EventHandler<ActionEvent>,e
     @Override
     public void handle(ActionEvent event) {
         throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public AnimationTimer getTimer(){
+        return timer;
     }
 }
 
