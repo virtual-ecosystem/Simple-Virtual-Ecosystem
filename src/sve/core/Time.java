744b1f798e0401ef3975732d80b2112e36ae064c
repelliuss <officialrepelliuss@gmail.com>
2020-06-01 18:09:08 +0300

    Extracted static methods

--- src/sve/core/Time.java
+++ src/sve/core/Time.java
@@ -2,28 +2,18 @@ package sve.core;
 
 public class Time {
 
-	private static long staticStart;
-	private static final double NANO_TO_SECONDS = 1000000000.0;
-
 	private long start;
+	private static final double NANO_TO_SECONDS = 1000000000.0;
 
 	public Time() {
 		start = System.nanoTime();
 	}
 
-	public static void checkpoint() {
-		staticStart = System.nanoTime();
-	}
-
-	public static double deltaTime() {
-		return (System.nanoTime() - staticStart) / NANO_TO_SECONDS;
-	}
-
-	public void refresh() {
+	public void checkpoint() {
 		start = System.nanoTime();
 	}
 
-	public double getDeltaTime() {
+	public double deltaTime() {
 		return (System.nanoTime() - start) / NANO_TO_SECONDS;
 	}
 }
