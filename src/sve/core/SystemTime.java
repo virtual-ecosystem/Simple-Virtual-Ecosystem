25754e787b0fda825f42885e1a61953d467b12a0
repelliuss <officialrepelliuss@gmail.com>
2020-06-01 18:10:11 +0300

    Added a system timer

---
+++ src/sve/core/SystemTime.java
@@ -0,0 +1,15 @@
+package sve.core;
+
+public class SystemTime {
+
+	private static long start = System.nanoTime();
+	private static final double NANO_TO_SECONDS = 1000000000.0;
+
+	public static void checkpoint() {
+		start = System.nanoTime();
+	}
+
+	public static double deltaTime() {
+		return (System.nanoTime() - staticStart) / NANO_TO_SECONDS;
+	}
+}
