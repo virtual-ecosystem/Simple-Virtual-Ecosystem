09fcb522402f77191b83c633cb6c103713d4c475
repelliuss <officialrepelliuss@gmail.com>
2020-06-08 14:43:29 +0300

    Added comments for EventManager

--- src/sve/event/EventManager.java
+++ src/sve/event/EventManager.java
@@ -5,13 +5,20 @@ import java.util.HashMap;
 
 public class EventManager {
 
-	//Will be changed with NavigableMap
+	//TODO Change this to NavigableMap
 	private Map<EventKey, Speaker> events;
 
 	public EventManager() {
 		this.events = new HashMap<>();
 	}
 
+	/**
+	 * Registers the listener to the given key
+	 * @param key an event key with event type and priority
+	 * @param listener that will be registered to given key
+	 * @return false if listener already registered to the given key,
+	 * true otherwise
+	 */
 	public boolean register(EventKey key, Listener listener) {
 
 		Speaker speaker = events.get(key);
@@ -22,6 +29,13 @@ public class EventManager {
 		return speaker.add(listener);
 	}
 
+	/**
+	 * Unregisters the listener from the given key
+	 * @param key an event key with event type and priority
+	 * @param listener that will be unregistered to given key
+	 * @return true if removed which happens if already registered,
+	 * false otherwise
+	 */
 	public boolean unregister(EventKey key, Listener listener) {
 
 		Speaker speaker = events.get(key);
@@ -32,18 +46,28 @@ public class EventManager {
 		return speaker.remove(listener);
 	}
 
-	public void onEvent(EventType type) {
+	/**
+	 * Calls the event for registered listeners foreach priority
+	 * @param type called event type
+	 */
 	public void callEvent(EventType type) {
 		//TODO for each event priority
 	}
 
-	public void onEvent(EventType type, EventPriority priority) {
-		onEvent(EventKey.valueOf(type, priority));
+
+	/**
+	 * Calls the event for registered listeners for given priority
+	 * @param type event type
+	 * @param priority event priority
+	 */
 	public void callEvent(EventType type, EventPriority priority) {
 		callEvent(EventKey.valueOf(type, priority));
 	}
 
-	public void onEvent(EventKey key) {
+	/**
+	 * Calls the event for registered listeners for given key
+	 * @param key event key with event type and priority
+	 */
 	public void callEvent(EventKey key) {
 
 		Speaker speaker = events.get(key);
