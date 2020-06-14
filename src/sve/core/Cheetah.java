6546d07ed0434be1470e9c83e3938f77ac19e636
Harun Albayrak <52447324+harunalbayrak@users.noreply.github.com>
2020-06-14 21:19:56 +0300

    Added Cheetah and Gazelle class

---
+++ src/sve/core/Cheetah.java
@@ -0,0 +1,60 @@
+package sve.core;
+
+import sve.gui.RenderTypes;
+
+public class Cheetah extends Animal {
+    @Override
+    public double speed() {
+        return 0;
+    }
+
+    @Override
+    public int age() {
+        return 0;
+    }
+
+    @Override
+    public int weight() {
+        return 0;
+    }
+
+    @Override
+    public int height() {
+        return 0;
+    }
+
+    @Override
+    public Sex sex() {
+        return null;
+    }
+
+    @Override
+    public double fertility() {
+        return 0;
+    }
+
+    @Override
+    public int heat() {
+        return 0;
+    }
+
+    @Override
+    public LifePeriod periodOfLife() {
+        return null;
+    }
+
+    @Override
+    public int resourceValue() {
+        return 0;
+    }
+
+    @Override
+    public ResourceType resourceType() {
+        return null;
+    }
+
+    @Override
+    public String pathTo(RenderTypes type) {
+        return "file:src/sve/gui/sample/images/64-cheetah.jpg";
+    }
+}
