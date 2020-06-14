2098284924d9f92bc1d1c0299461da771f5817ee
Harun Albayrak <52447324+harunalbayrak@users.noreply.github.com>
2020-06-14 21:25:49 +0300

    Added these classes' stubs

--- src/sve/core/Gazelle.java
+++ src/sve/core/Gazelle.java
@@ -5,51 +5,61 @@ import sve.gui.RenderTypes;
 public class Gazelle extends Animal {
     @Override
     public double speed() {
+        // TODO Auto-generated method stub
         return 0;
     }
 
     @Override
     public int age() {
+        // TODO Auto-generated method stub
         return 0;
     }
 
     @Override
     public int weight() {
+        // TODO Auto-generated method stub
         return 0;
     }
 
     @Override
     public int height() {
+        // TODO Auto-generated method stub
         return 0;
     }
 
     @Override
     public Sex sex() {
+        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public double fertility() {
+        // TODO Auto-generated method stub
         return 0;
     }
 
     @Override
     public int heat() {
+        // TODO Auto-generated method stub
         return 0;
     }
 
     @Override
     public LifePeriod periodOfLife() {
+        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public int resourceValue() {
+        // TODO Auto-generated method stub
         return 0;
     }
 
     @Override
     public ResourceType resourceType() {
+        // TODO Auto-generated method stub
         return null;
     }
 
