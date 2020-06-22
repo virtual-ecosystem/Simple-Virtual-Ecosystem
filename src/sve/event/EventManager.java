c592f2f53b7c0247517f9bb1bf4a8751bc5cdbf1
repelliuss <officialrepelliuss@gmail.com>
2020-06-23 02:33:00 +0300

    Removed priorities of events
    We call speakers directly so there is no direct utility
    for priorities

--- src/sve/event/EventManager.java
+++ src/sve/event/EventManager.java
@@ -6,39 +6,39 @@ import java.util.HashMap;
 public class EventManager {
 
 	//TODO Change this to NavigableMap
-	private Map<EventKey, Speaker> events;
+	private Map<EventType, Speaker> events;
 
 	public EventManager() {
 		this.events = new HashMap<>();
 	}
 
 	/**
-	 * Registers the listener to the given key
-	 * @param key an event key with event type and priority
-	 * @param listener that will be registered to given key
-	 * @return false if listener already registered to the given key,
+	 * Registers the listener to the given event type
+	 * @param etype an event type
+	 * @param listener that will be registered to given event type
+	 * @return false if listener already registered to the given event type,
 	 * true otherwise
 	 */
-	public boolean register(EventKey key, Listener listener) {
+	public boolean register(EventType etype, Listener listener) {
 
-		Speaker speaker = events.get(key);
+		Speaker speaker = events.get(etype);
 		if(speaker == null) {
-			speaker = events.put(key, new Speaker());
+			speaker = events.put(etype, new Speaker());
 		}
 
 		return speaker.add(listener);
 	}
 
 	/**
-	 * Unregisters the listener from the given key
-	 * @param key an event key with event type and priority
-	 * @param listener that will be unregistered to given key
+	 * Unregisters the listener from the given event type
+	 * @param etype an event type
+	 * @param listener that will be unregistered to given event type
 	 * @return true if removed which happens if already registered,
 	 * false otherwise
 	 */
-	public boolean unregister(EventKey key, Listener listener) {
+	public boolean unregister(EventType etype, Listener listener) {
 
-		Speaker speaker = events.get(key);
+		Speaker speaker = events.get(etype);
 		if(speaker == null) {
 			return false;
 		}
@@ -47,35 +47,15 @@ public class EventManager {
 	}
 
 	/**
-	 * Calls the event for registered listeners foreach priority
-	 * @param type called event type
+	 * Calls the event for registered listeners for given event type
+	 * @param etype an event type
 	 */
-	public void callEvent(EventType type) {
-		for(EventPriority priority : EventPriority.values()) {
-			callEvent(EventKey.valueOf(type, priority));
-		}
-	}
+	public void callEvent(EventType etype) {
 
-
-	/**
-	 * Calls the event for registered listeners for given priority
-	 * @param type event type
-	 * @param priority event priority
-	 */
-	public void callEvent(EventType type, EventPriority priority) {
-		callEvent(EventKey.valueOf(type, priority));
-	}
-
-	/**
-	 * Calls the event for registered listeners for given key
-	 * @param key event key with event type and priority
-	 */
-	public void callEvent(EventKey key) {
-
-		Speaker speaker = events.get(key);
+		Speaker speaker = events.get(etype);
 		if(speaker == null)
 			return;
 
-		speaker.speak(key.type);
+		speaker.speak(etype);
 	}
 }
