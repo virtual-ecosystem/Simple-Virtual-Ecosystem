21c7154f39028764c8345a28d518a4a8c63c762c
repelliuss <officialrepelliuss@gmail.com>
2020-06-24 02:39:23 +0300

    Added animals to database

--- src/sve/core/SVEDatabase.java
+++ src/sve/core/SVEDatabase.java
@@ -1,10 +1,21 @@
 package sve.core;
 
+import java.util.ArrayList;
+
 /**
  * Database class of SVE
  *
  * @author repelliuss
  */
 public class SVEDatabase {
-    
+
+	private ArrayList<Animal> animals;
+
+	public SVEDatabase(ArrayList<Animal> animals) {
+		this.animals = animals;
+	}
+
+	public ArrayList<Animal> getAnimals() {
+		return animals;
+	}
 }
